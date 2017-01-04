36bf63b38ea779c7735185b2992a2f9c0904c8c4
Ranga Rao Karanam <rangaraokaranam@Rangas-MacBook-Pro.local>
2017-01-04 09:30:57 +0530

    Step 017

--- src/main/java/com/in28minutes/springboot/Application.java
+++ src/main/java/com/in28minutes/springboot/Application.java
@@ -1,5 +1,8 @@
 package com.in28minutes.springboot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +11,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.springboot.configuration.BasicConfiguration;
 
 @SpringBootApplication
 public class Application {
@@ -23,9 +28,22 @@ public class Application {
         @Autowired
         private SomeDependency someDependency;
 
+        @Autowired
+        private BasicConfiguration configuration;
+
         @RequestMapping("/")
         public String index() {
             return someDependency.getSomething();
+        }
+
+        @RequestMapping("/dynamic-configuration")
+        public Map dynamicConfiguration() {
+            // Not the best practice to use a map to store differnt types!
+            Map map = new HashMap();
+            map.put("message", configuration.getMessage());
+            map.put("number", configuration.getNumber());
+            map.put("key", configuration.isValue());
+            return map;
         }
 
     }
