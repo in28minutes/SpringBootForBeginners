ca6943a0aa9a97d64ea55d6b1b352dc70695e864
IsthiyakSaalim <95282599+IsthiyakSaalim@users.noreply.github.com>
2022-03-02 10:36:08 +0530

    Added 2 new books

--- 01.Spring-Boot-Introduction-In-10-Steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
+++ 01.Spring-Boot-Introduction-In-10-Steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
@@ -11,6 +11,8 @@ public class BooksController {
 	@GetMapping("/books")
 	public List<Book> getAllBooks() {
 		return Arrays.asList(
-				new Book(1l, "Mastering Spring 5.2", "Isthiyak Saalim M"));
+				new Book(1l, "Mastering Spring 5.2", "Isthiyak Saalim M"), 
+				new Book(2l, "Mastering Spring Boot", "Nawrin Banu S"), 
+				new Book(3l, "Mastering Python", "Lorem Ipsum M"));
 	}
 }
