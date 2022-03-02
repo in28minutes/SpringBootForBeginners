f7db7f465c48c9da10e1b02b622c0f8b4f72884a
Isthiyak <salim2592@gmail.com>
2022-03-02 10:30:51 +0530

    Added initial

--- 01.Spring-Boot-Introduction-In-10-Steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
+++ 01.Spring-Boot-Introduction-In-10-Steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
@@ -11,6 +11,6 @@ public class BooksController {
 	@GetMapping("/books")
 	public List<Book> getAllBooks() {
 		return Arrays.asList(
-				new Book(1l, "Mastering Spring 5.2", "Isthiyak Saalim"));
+				new Book(1l, "Mastering Spring 5.2", "Isthiyak Saalim M"));
 	}
 }
